5dea5a49f76db948fe3ff9878303a4218589c687
muzammilshaikh12 <110051885+muzammilshaikh12@users.noreply.github.com>
2022-09-12 11:01:44 +0530

    Task 4

--- controller/user.js
+++ controller/user.js
@@ -13,3 +13,28 @@ exports.signup = (req,res,next)=>{
     .then(res.status(201).json({message:'User Successfully Created'}))
     .catch(err=>res.status(500).json({message:'Something went wrong'}))
 }
+
+exports.login=(req,res,next) =>{
+    const{email,password} = req.body
+    if(email == undefined || email.length === 0
+        || password == undefined || password.length === 0)
+        {
+            return res.status(400).json({err:'Email Id or Password Missing',success:false})
+        }
+        User.findAll({where:{email}})
+        .then(user=>{
+            if(user.length>0){
+                if(user[0].password === password){
+                    res.status(200).json({message:'Successfully logged in', success:true})
+                } else {
+                    res.status(400).json({message: 'Password did not match', success:false})
+                }
+            } else {
+                res.status(404).json({message:'User does not exist'})
+            }
+        })
+
+        .catch(err=>{
+            res.status(500).json({message:err, success:false})
+        })
+}
--- models/user.js
+++ models/user.js
@@ -14,7 +14,11 @@ const User = sequelize.define('user', {
     type:Sequelize.STRING,
     allowNull:false,
     unique : true
-  } 
+  },
+  password:{
+    type:Sequelize.STRING,
+    allowNull:false
+  }
 });
 
 module.exports = User;
